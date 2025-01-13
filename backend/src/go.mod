e4e6e9781e4204d3ad5995057c2243225e32ac9a
Yghore <yhgore@gmail.com>
2025-01-13 21:58:02 +0100

    add: prototypage webdav

---
+++ backend/src/go.mod
@@ -0,0 +1,5 @@
+module github.com/KiwiGDC/KDav
+
+go 1.23
+
+require golang.org/x/net v0.31.0 // indirect
---
+++ backend/src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/webdav"
+)
+
+func logger(r *http.Request, err error) {
+	if err != nil {
+		fmt.Printf("Request: %s, Method: %s, Error: %v\n", r.URL.Path, r.Method, err)
+	} else {
+		fmt.Printf("Request: %s, Method: %s\n", r.URL.Path, r.Method)
+	}
+}
+
+func digestAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		println(authHeader)
+		if authHeader == "" {
+			w.Header().Set("WWW-Authenticate", "Digest realm=\"Webdav\"")
+			http.Error(w, "Unauthorized : Authorization header is empty", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	handler := &webdav.Handler{
+		FileSystem: webdav.Dir(`/tmp/test`),
+		LockSystem: webdav.NewMemLS(),
+		Logger:     logger,
+	}
+	print("Start WebDav server")
+	http.ListenAndServe("localhost:8080", digestAuthMiddleware(handler))
+}
